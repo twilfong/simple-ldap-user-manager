9ba8b41cb044b587def467c28568dac9e19d49a0
integrii <integrii@gmail.com>
2014-06-10 15:18:41 -0700

    Adding forms for removal and addition to ldap groups

---
+++ remove-from-group.php
@@ -0,0 +1,49 @@
+<?php
+
+// Include configuration and functions
+require_once('config.inc');
+require_once('functions.inc');
+require_once('ldapconnect.inc');
+
+// Require SSL if configured to
+if($requireSSL === TRUE){
+	require_ssl();
+}
+
+
+
+/*
+GET/POST INPUT VARIABLES:
+cn - group name
+uid - user name
+
+
+*/
+
+
+
+
+
+// If we are given POST data, intercept it and act
+if(isset($_REQUEST['cn'])){
+
+	// Collect post variables for group creation
+	// TODO: Input validation
+	$cn = $_REQUEST['cn'];
+	$uid = $_REQUEST['uid'];
+
+	// Start LDAP connection
+	$connection = new ldapConnection();
+
+	// Create new ldap group
+	$connection->removeFromGroup($uid,$cn);
+
+	echo "Done! $uid added to $cn"
+
+} else { 
+	include('group-remove-form.inc');
+
+} // end else post operation is not set
+
+
+?>
