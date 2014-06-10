3adff95eee9142a44986755d247afd6639942700
integrii <integrii@gmail.com>
2014-06-10 15:26:24 -0700

    Adding index, forgtten include and fixes for group removal and addition

---
+++ index.php
@@ -0,0 +1,4 @@
+<a href='add-user.php'>Create User</a><br />
+<a href='add-group.php'>Create Group</a><br />
+<a href='add-to-group.php'>Add User to Group</a><br />
+<a href='remove-from-group.php'>Remove User from Group</a><br />
